131c59f7b2f514b9ed6160eb70b538728829a3f4
SoponSaenkaew <soponsaenkaew@gmail.com>
2026-04-21 23:38:17 +0700

    Add imagePath accessor for image URL handling
    Add imagePath accessor to retrieve image URL from S3 or return as is.

--- app/Models/PostImage.php
+++ app/Models/PostImage.php
@@ -18,4 +18,11 @@ class PostImage extends Model
     {
         return $this->belongsTo(Post::class);
     }
+    protected function imagePath(): Attribute
+    {
+        return Attribute::get(function ($value) {
+            // ถ้ามันขึ้นต้นด้วย http อยู่แล้วก็ส่งคืนเลย แต่ถ้าไม่ ก็ให้ดึง URL จาก S3
+            return str_starts_with($value, 'http') ? $value : Storage::disk('s3')->url($value);
+        });
+    }
 }

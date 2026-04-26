f4be9c6f94a6eb0266987fb58176735e0b1165ef
SoponSaenkaew <soponsaenkaew@gmail.com>
2026-04-27 02:42:51 +0700

    Optimize Figtree font loading in app.blade.php
    Updated the link tag for Figtree font to load with media attribute for improved performance.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net" crossorigin>
         <link rel="dns-prefetch" href="https://fonts.bunny.net">
         
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700,900&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://fonts.bunny.net/css?family=figtree:400,500,600,700,900&display=swap" 
+      media="print" onload="this.media='all'">
 
         @routes
         @viteReactRefresh

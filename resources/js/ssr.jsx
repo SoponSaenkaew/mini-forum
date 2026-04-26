3978bb426fa25f8551ee2eaa61cbf6da63247e5b
Sopon <soponsaenkaew@gmail.com>
2026-04-27 01:32:24 +0700

    perf: improve speed index via app shell optimization and ssr setup

---
+++ resources/js/ssr.jsx
@@ -0,0 +1,24 @@
+import { createInertiaApp } from '@inertiajs/react';
+import createServer from '@inertiajs/react/server';
+import ReactDOMServer from 'react-dom/server';
+import { route } from '../../vendor/tightenco/ziggy';
+
+createServer((page) =>
+    createInertiaApp({
+        page,
+        render: ReactDOMServer.renderToString,
+        resolve: (name) => {
+            const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true });
+            return pages[`./Pages/${name}.jsx`];
+        },
+        setup: ({ App, props }) => {
+            global.route = (name, params, absolute) =>
+                route(name, params, absolute, {
+                    ...page.props.ziggy,
+                    location: new URL(page.props.ziggy.location),
+                });
+
+            return <App {...props} />;
+        },
+    })
+);

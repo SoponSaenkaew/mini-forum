8379343ee0936561c202cf505d7c2485fd1b025b
Sopon <soponsaenkaew@gmail.com>
2026-04-28 01:40:36 +0700

    Fix:PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -164,9 +164,7 @@ class PostController extends Controller
                 'comments' => function($query) {
                     $query->whereNull('parent_id') // ดึงเฉพาะคอมเมนต์หลัก (ไม่ใช่การตอบกลับ)
                           ->with(['user', 'likes', 'replies.user', 'replies.likes'])
-                          ->latest()
-                          ->get()
-                          ->values()
+                          ->latest();
                 }
             ]),
             'highlightId' => $highlightId ? (int)$highlightId : null,

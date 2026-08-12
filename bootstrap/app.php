bb544e27e8bff6618224a0e5440278bd3cadf5fb
SoponSaenkaew <soponsaenkaew@gmail.com>
2026-08-13 01:34:19 +0700

    Configure middleware to trust proxies in app.php
    Added middleware configuration to trust proxies and headers.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request; // 1. Import Request เข้ามาใช้งาน
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,12 +13,20 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // 2. ตั้งค่าให้ Laravel เชื่อถือ Trust Proxies จาก Render
+        $middleware->trustProxies(at: '*');
+        $middleware->trustHeaders(at: 
+            Request::HEADER_X_FORWARDED_FOR | 
+            Request::HEADER_X_FORWARDED_HOST | 
+            Request::HEADER_X_FORWARDED_PORT | 
+            Request::HEADER_X_FORWARDED_PROTO | 
+            Request::HEADER_X_FORWARDED_AWS_ELB
+        );
+
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
-        //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
